e72b8db6581105f06284df7c2ed5e1332dff5849
Jeremy Glebe <jeremyglebe@gmail.com>
2018-11-26 11:08:27 -0600

    package files and junk.

--- StalkerApp/src/pages/login/login.ts
+++ StalkerApp/src/pages/login/login.ts
@@ -1,3 +1,4 @@
+import { DatabaseProvider } from './../../providers/database/database';
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
@@ -20,8 +21,9 @@ export class LoginPage {
   constructor(
     public navCtrl: NavController,
     public formBuilder: FormBuilder,
-    public auth: AuthProvider
-  ) {}
+    public auth: AuthProvider,
+    public database: DatabaseProvider
+  ) { }
 
 
   ionViewWillLoad() {
@@ -39,7 +41,6 @@ export class LoginPage {
     } catch (e) {
       console.log(e);
     }
-
   }
 
   async tryLoginWithGoogle() {
