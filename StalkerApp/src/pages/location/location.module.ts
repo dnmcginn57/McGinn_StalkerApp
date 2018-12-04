fb1191f451f0f213ec80a17a4e0ff237f609529d
Jamada623 <jamal2501@hotmail.com>
2018-12-04 15:37:38 -0600

    changes

--- StalkerApp/src/pages/location/location.module.ts
+++ StalkerApp/src/pages/location/location.module.ts
@@ -2,6 +2,12 @@ import { ErrorHandler,NgModule } from '@angular/core';
 import { IonicPageModule, IonicErrorHandler } from 'ionic-angular';
 import { LocationPage } from './location';
 
+import { LocationTracker } from '../../providers/location-tracker/location-tracker';
+
+import { Geolocation } from '@ionic-native/geolocation';
+import { BrowserModule } from '@angular/platform-browser';
+import {BackgroundGeolocation} from '@ionic-native/background-geolocation';
+
 
 @NgModule({
   declarations: [
